4d4c04399aab245faf57b3be36b9f0a14448dd5f
Frammco19 <fa141359@gmail.com>
2025-09-30 13:47:16 -0300

    Controller for check deploy

---
+++ ConversorAPI/ConversorAPI/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConversorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : Controller
+    {
+        
+       [HttpGet]
+       public IActionResult Ping()
+       {
+          return Ok("ConversorAPI is running");
+       }
+        
+    }
+}
